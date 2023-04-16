0a931dd4cd6f48377c5a5d63a35651b98737d44f
vuminhphuong <vuminhphuong1992@gmail.com>
2023-04-16 22:05:43 +0700

    Longdesign

---
+++ Longdesign/index.js
@@ -0,0 +1,11 @@
+const titles = document.getElementsByClassName("title-tag");
+const tags = document.getElementsByClassName("tag");
+for (let i = 0; i < tags.length; i++) {
+  tags[i].addEventListener("click", function() {
+    const current = document.getElementsByClassName("tag-active");
+    current[0].className = current[0].className.replace(" tag-active", "");
+    this.className += " tag-active";
+  });
+}
+
+console.log(tags);
